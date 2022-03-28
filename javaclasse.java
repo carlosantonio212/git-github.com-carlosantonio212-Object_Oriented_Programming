10bf344e39cae11c107e9b102cb31460654218e6
Carlos Antônio <95361309+carlosantonio212@users.noreply.github.com>
2022-03-28 12:07:07 +0000

    hello

---
+++ javaclasse.java
@@ -0,0 +1,15 @@
+package aulaclasse;
+
+public class javaclasse {
+
+	public static void main(String[] args) {
+		System.out.println("Trabalho de classes");
+		System.out.println("Valor maxímo que uma variavel do tipo Double pode suporta: "+Double.MAX_VALUE);
+		System.out.println("\nOperação logica Xor entre dois valores Booleanos: "+Boolean.logicalXor(true, false));
+		System.out.println("\nConversão de um numero Inteiro em Binário: "+Integer.toBinaryString(10));
+		System.out.println("\nConversão de um número Inteiro em Hexadecimal: "+Integer.toHexString(10));
+		System.out.println("\nConversão de um número Inteiro em Octal: "+Integer.toOctalString(10));
+		System.out.println("\nRetorna o Maior valor entre dois Floats: "+Float.max(10, 15));
+	}
+
+}
